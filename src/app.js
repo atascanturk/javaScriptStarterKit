fda7cd9764711647aa5d7fde5461f031383619d5
atascanturk <atascanturk@gmail.com>
2021-10-20 00:19:13 +0300

    first commit

---
+++ src/app.js
@@ -0,0 +1,11 @@
+console.log("Hello world!")
+
+//let is valid in the its same block.
+let dolarDun = 9.20;
+let dolarBugun = 9.30;
+
+console.log(dolarDun);
+
+//const means readonly
+const euroDun = 11.2
+
